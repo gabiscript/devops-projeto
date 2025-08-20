f623db4f5171ab7f32fd0dbfe2c35851121c6c43
Gabriela <gabrielamedsants@gmail.com>
2025-08-20 15:05:58 -0300

    Adicionar API e testes

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def home():
+    return jsonify(message="Olá! Esta é uma API da matéria de DevOps.")
+
+@app.route('/status', methods=['GET'])
+def status():
+    return jsonify(status="UP", message="tudo funcionando!!!")
+
+@app.route('/about', methods=['GET'])
+def about():
+    return jsonify(project="DevOps Fase 1", author="Gabi", version="1.0")
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
---
+++ test_app.py
@@ -0,0 +1,18 @@
+from app import app
+
+def test_status():
+    response = app.test_client().get('/status')
+    assert response.status_code == 200
+    assert response.get_json()['status'] == "UP"
+
+def test_home():
+    response = app.test_client().get('/')
+    assert response.status_code == 200
+    assert "API da matéria" in response.get_json()['message']
+
+def test_about():
+    response = app.test_client().get('/about')
+    assert response.status_code == 200
+    data = response.get_json()
+    assert data['project'] == "DevOps Fase 1"
+    assert data['author'] == "Gabi"
