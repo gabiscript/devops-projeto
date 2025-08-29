2ab7e4d5b5b583797d6c9216e2be43779f0944cc
Gabriela <gabrielamedsants@gmail.com>
2025-08-29 14:36:54 -0300

    teste do ci/cd

--- test_app.py
+++ test_app.py
@@ -3,7 +3,7 @@ from app import app
 def test_status():
     response = app.test_client().get('/status')
     assert response.status_code == 200
-    assert response.get_json()['status'] == "UP"
+    assert response.get_json()['status'] == "OK"
 
 def test_home():
     response = app.test_client().get('/')
